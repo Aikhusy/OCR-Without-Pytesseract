b4c38646be6c77ecbd75bf2707507dd9afab573c
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-12-12 17:20:37 +0700

    otw Trackbars

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,50 @@
-def handler():
+import os
+import glob
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def pathProcessing (path):
+    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif']  # Add more extensions if needed
+    image_paths = []
+
+    for ext in image_extensions:
+        image_paths.extend(glob.glob(os.path.join(path, ext)))
+
+    return image_paths
+
+def readImage (paths):
+    images=[]
+
+    for path in paths:
+        image= cv2.imread(path,0)
+        if image is not None:
+            images.append(image)
+        else:
+            print(f"Failed to read image at path: {path}")
+
+    return images
+
+def gaussianBlur(images):
+    blurredImages=[]
+    for image in images:
+        kernel_size = (5, 5)
+        sigma_x = 1
+        blurredImage = cv2.GaussianBlur(image, kernel_size, sigma_x)
+        blurredImages.append(blurredImage)
+
+    return blurredImages
+
+def handler(imageFolders):
     #import image
-    
+    readyPaths= pathProcessing(imageFolders)
+    pureImages= readImage(readyPaths)
+
+    processingImages= pureImages
     #blur image
+    bluredImages= gaussianBlur(processingImages)
+    
     #trackbars
     #canny edge detect
     #dilate 
