4d019d340dd4fe1a8f12743b39ff76acf5910aac
Syaikhul Shafwan R <heymepoo@gmail.com>
2023-12-13 22:44:31 +0700

    pusyingg

--- preprocessing.py
+++ preprocessing.py
@@ -44,6 +44,33 @@ def gaussianBlur(images):
 
     return blurredImages
 
+def brighten_image(images, alpha=0.5, beta=10):
+    bright=[]
+    for image in images:
+        brightened_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+        bright.append(brightened_image)
+
+    return bright
+
+def otsuThresholding(images):
+    # List to store the thresholded (binary) images
+    binary_images = []
+
+    for image in images:
+        # Convert the image to grayscale if it's not already
+        if len(image.shape) == 3:
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        else:
+            gray_image = image
+
+        # Apply Otsu's thresholding
+        _, binary_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # Append the binary image to the list
+        binary_images.append(binary_image)
+
+    return binary_images
+
 def cannyEdgeDetect(images,th1,th2):
     edgedImages = []
 
@@ -215,12 +242,12 @@ def display_images(images, titles):
     # Menghitung jumlah baris dan kolom
     rows = len(images) // 2 + len(images) % 2
     cols = 2
-    dim =180
+    dim = 180
     # Membuat frame kosong dengan ukuran yang sesuai
-    frame = np.zeros((rows * dim, cols * dim), dtype=np.uint8)
+    frame = np.zeros((rows * dim, cols * dim, 3), dtype=np.uint8)
 
     for i, (img, title) in enumerate(zip(images, titles)):
-        # Resizing gambar menjadi (500, 500)
+        # Resizing gambar menjadi (dim, dim)
         resized_img = cv2.resize(img, (dim, dim))
 
         # Menentukan baris dan kolom untuk menempatkan gambar
@@ -231,12 +258,51 @@ def display_images(images, titles):
         frame[row * dim:(row + 1) * dim, col * dim:(col + 1) * dim] = resized_img
 
         # Menambahkan judul di bawah gambar
-        cv2.putText(frame, title, (col * dim, (row + 1) * dim - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255, 1, cv2.LINE_AA)
+        cv2.putText(frame, title, (col * dim, (row + 1) * dim - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
     # Menampilkan frame yang berisi semua gambar
     cv2.imshow('Image Frame', frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def resize_image(image, size=(200, 200)):
+    """
+    Resize an image to the specified size.
+
+    Parameters:
+    - image: The input image to be resized.
+    - size: A tuple representing the desired size (width, height).
+
+    Returns:
+    - Resized image.
+    """
+    return cv2.resize(image, size)
+
+def displayImages(images, titles):
+    """
+    Display a list of images with corresponding titles using Matplotlib.
+
+    Parameters:
+    - images: List of images to be displayed.
+    - titles: List of titles for each image.
+    """
+    num_images = len(images)
+
+    # Create subplots based on the number of images
+    fig, axes = plt.subplots(1, num_images, figsize=(15, 5))
+
+    for i in range(num_images):
+        # Resize the image to 200x200 pixels
+        resized_image = resize_image(images[i], size=(200, 200))
+
+        # If the image is in grayscale, use cmap='gray'
+        cmap = 'gray' if len(resized_image.shape) == 2 else None
+
+        axes[i].imshow(resized_image, cmap=cmap)
+        axes[i].set_title(titles[i])
+        axes[i].axis('off')
+
+    plt.show()
 
 def handler(imageFolders):
     #import image
@@ -273,18 +339,45 @@ def handler2(path):
 
     grayedImages= grayImages(pureImages)
 
-    blurredImages=gaussianBlur(grayedImages)
+    bright= brighten_image(grayedImages)
+
+    blurredImages=gaussianBlur(bright)
 
     edged= cannyEdgeDetect(blurredImages,50,100)
 
-    flip= contourIndia(pureImages,edged)
 
     number=5
 
     # Lakukan resizing menggunakan cv2.resize
-    images_to_display = [pureImages[number], grayedImages[number], blurredImages[number], edged[number], flip[number]]
+    images_to_display = [pureImages[number], grayedImages[number], blurredImages[number], edged[number], bright[number]]
     titles = ['Pure Image', 'Blurred Image', 'Segmented Image', 'Opened Image', 'Dilated Contour']
 
     display_images(images_to_display,titles)
 
-handler2('Dataset')
+
+def handler3(path):
+    processed= pathProcessing(path)
+
+    pureImages=readImage(processed)
+
+    processedImage=pureImages
+
+    grayed=grayImages(processedImage)
+
+    blured=gaussianBlur(grayed)
+
+    canny=cannyEdgeDetect(blured,50,100)
+
+    erosi=erode(canny)
+
+    kuntur=contour(pureImages,erosi)
+
+    number=10
+
+    images_to_display = [pureImages[number], grayed[number], blured[number], erosi[number],canny[number],kuntur[number]]
+    titles = ['Pure Image', 'Gray', 'Blurred','eroded','canny',"kuntur"]
+
+    displayImages(images_to_display,titles)
+    
+
+handler3('Dataset')
